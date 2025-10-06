5a216d0b13bff368c2683df4556b6c2a219a6d57
Tameka Roberson <tamekaroberson1995@gmail.com>
2025-10-05 20:58:04 -0400

    Persons & Courses Class(es) Updated

--- src/Courses/Course.java
+++ src/Courses/Course.java
@@ -1,5 +1,31 @@
 package Courses;
 
 public class Course {
+    public String courseName;
+    public int courseId = 100;
+
+    public Course(){};
+
+    public Course(String courseName, int courseId){
+        setCourseName(courseName);
+        setCourseId();
+    }
+
+    public void setCourseName(String newCourseName){
+        if(newCourseName.length() != 0 && newCourseName != null){
+            this.courseName = newCourseName;
+        }
+    }
+
+    public void setCourseId(){
+        this.courseId = courseId++;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
     
+    public int getCourseId(){
+        return courseId;
+    }
 }
--- src/Persons/Person.java
+++ src/Persons/Person.java
@@ -1,6 +1,52 @@
 package Persons;
 
 public class Person {
-    
+    private String name;
+    private int age;
+    private String email;
+
+    public Person(){};
+
+    public Person(String name, int age, String email){
+        setName(name);
+        setEmail(email);
+        setAge(age);
+    }
+
+    public void setName(String newName){
+        if(newName.length() != 0 && newName != null && newName.matches("[a-zA-Z\\s]+")){
+            this.name = newName;
+        }else{
+            throw new IllegalArgumentException("Name must contain only alphabetical characters and spaces");
+        } 
+    }
+
+    public void setEmail(String newEmail){
+        if(newEmail.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$") && newEmail != null && newEmail.length() != 0){
+           this.email = newEmail; 
+        }else{
+            throw new IllegalArgumentException("Email must follow appropriate format and not be empty or null");
+        }
+    }
+
+    public void setAge(int newAge){
+        if(newAge > 0){
+            this.age = newAge;
+        }else{
+            throw new IllegalArgumentException("Age must be greater than 0");
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getAge(){
+        return age;
+    }
 
 }
